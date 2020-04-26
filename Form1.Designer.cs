89a1b4ad7837c62d0f8288fee62cb6e28e9ca411
wesleyiab <61994864+wesleyiab@users.noreply.github.com>
2020-04-26 10:05:30 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Formulario2704
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button1Entrar = new System.Windows.Forms.Button();
+            this.button2Sair = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(104, 47);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(158, 20);
+            this.txtUser.TabIndex = 2;
+            this.txtUser.TextChanged += new System.EventHandler(this.textBox1User_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(104, 86);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(158, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // button1Entrar
+            // 
+            this.button1Entrar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1Entrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1Entrar.Location = new System.Drawing.Point(70, 145);
+            this.button1Entrar.Name = "button1Entrar";
+            this.button1Entrar.Size = new System.Drawing.Size(75, 23);
+            this.button1Entrar.TabIndex = 4;
+            this.button1Entrar.Text = "Entrar";
+            this.button1Entrar.UseVisualStyleBackColor = false;
+            this.button1Entrar.Click += new System.EventHandler(this.button1Entrar_Click);
+            // 
+            // button2Sair
+            // 
+            this.button2Sair.Location = new System.Drawing.Point(214, 145);
+            this.button2Sair.Name = "button2Sair";
+            this.button2Sair.Size = new System.Drawing.Size(75, 23);
+            this.button2Sair.TabIndex = 5;
+            this.button2Sair.Text = "Sair";
+            this.button2Sair.UseVisualStyleBackColor = true;
+            this.button2Sair.Click += new System.EventHandler(this.button2Sair_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(150, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Login";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 229);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2Sair);
+            this.Controls.Add(this.button1Entrar);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button button1Entrar;
+        private System.Windows.Forms.Button button2Sair;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario2704
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChar = '*';
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1User_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button1Entrar_Click(object sender, EventArgs e)
+        {
+            String name = txtUser.Text;
+            String Password = txtPassword.Text;
+            if (name == "" || Password == "") {
+                MessageBox.Show("Todos os campos devem estar preenchidos para poder logar!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (name != "" && Password == "") {
+                MessageBox.Show("Por favor digite sua senha para poder entrar!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPassword.Focus();
+            } else if (txtPassword.Text.Trim().Length < 8){
+                MessageBox.Show("A senha deve conter pelo menos 8 dígitos", "Atenção!",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                txtPassword.Focus();
+            }else {
+                MessageBox.Show("Login efetuado com sucesso!", "Parabéns", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2Sair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
